af54a4e269895d53ed3800608cc7575fe1cdfa91
Mackenzie Niswonger <mackenzie.niswonger@hope.edu>
2024-05-20 16:23:03 -0400

    Main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'measure.dart';
-
-void main() {
-  runApp(const MaterialApp(home: MeasureScreen()));
-=======
 import 'welcome_page.dart';
 
 void main() {
   runApp(const MaterialApp(home: WelcomePage()));
->>>>>>> bd043399df1eb97e03c005f01ad3d5557b071ee1
 }
