8ae7cc3f477b13bcd0068503861e85c205db6f1e
Chris <christopher.laubach@hope.edu>
2024-05-21 14:43:34 -0400

    first 4 pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:grip_fixer/connect_to_sensor.dart';
-import 'package:grip_fixer/player_selection.dart';
-import 'package:grip_fixer/new_player_page.dart';
 import 'welcome_page.dart';
 import 'shot_selection.dart';
 import 'package:go_router/go_router.dart';
