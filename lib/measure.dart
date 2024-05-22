1fc5b8688cc2f0b27e486b0c06f42dbcd7cb8374
Mackenzie Niswonger <mackenzie.niswonger@hope.edu>
2024-05-22 13:49:22 -0400

    ui adjustments on measure page

--- lib/measure.dart
+++ lib/measure.dart
@@ -10,47 +10,50 @@ class MeasureScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          const Align(
-            alignment: Alignment.topLeft,
-            child: Text(
-              "Grip Strength Tool",
-              style: TextStyle(fontSize: 42, fontWeight: FontWeight.bold),
-            ),
-          ),
-          const Align(
-            alignment: Alignment.topLeft,
-            child: Text(
-              "Measure Grip Strength",
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            ),
-          ),
-          const Align(
-            alignment: Alignment.topLeft,
-            child: Text(
-              "When you click Start, the player should grip the racket with their dominant hand as tightly as possible for a period of 5 seconds.",
-              style: TextStyle(fontSize: 18),
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () => context.go("/ShotSelectionPage"),
-            style: ElevatedButton.styleFrom(
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.zero, // Make the button square
+        child: Container(
+          margin: const EdgeInsets.only(left: 12, right: 12, top: 20),
+          child: Column(children: [
+            const Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                "Grip Strength Tool",
+                style: TextStyle(fontSize: 42, fontWeight: FontWeight.bold),
               ),
             ),
-            child: const Text(
-              'Let\'s Hit!',
-              style:
-                  TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+            const Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                "Measure Grip Strength",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
             ),
-          ),
-          const SizedBox(height: 16.0),
-          const Icon(
-            Icons.timer,
-            size: 130,
-          ),
-        ]),
+            const Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                "When you click Start, the player should grip the racket with their dominant hand as tightly as possible for a period of 5 seconds.",
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () => context.go("/ShotSelectionPage"),
+              style: ElevatedButton.styleFrom(
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.zero, // Make the button square
+                ),
+              ),
+              child: const Text(
+                'Let\'s Hit!',
+                style:
+                    TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            const Icon(
+              Icons.timer,
+              size: 130,
+            ),
+          ]),
+        ),
       ),
     );
   }
