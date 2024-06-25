3958ef8a583d41c765d41de1d0d35b3b4199eb8c
Chris <christopher.laubach@hope.edu>
2024-06-25 13:42:22 -0400

    bluetooth checks

--- lib/video_recording.dart
+++ lib/video_recording.dart
@@ -1,7 +1,6 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
-import 'package:grip_fixer/connect_to_sensor.dart';
 import 'package:grip_fixer/state.dart';
 import 'package:provider/provider.dart';
 import 'package:grip_fixer/session_measurements.dart';
@@ -22,10 +21,12 @@ class _VideoRecorderScreenState extends State<VideoRecorderScreen> {
   bool _isLoading = true;
   AppState state = AppState();
   BluetoothCharacteristic? responseCharacteristic;
+  final Completer<void> completer = Completer();
   int? currentResponseValue;
   Timer? _characteristicTimer;
   String? _videoFilePath;
   bool violating = false;
+  bool isConnecting = false;
 
   @override
   void initState() {
@@ -98,11 +99,11 @@ class _VideoRecorderScreenState extends State<VideoRecorderScreen> {
         currentResponseValue = sessionMeasurements.value;
         saveToDatabase(state);
         _characteristicTimer?.cancel();
-        _characteristicTimer = Timer(const Duration(seconds: 3), _onCharacteristicTimeout);
+        _characteristicTimer = Timer(const Duration(seconds: 2), _onCharacteristicTimeout);
       });
       responseCharacteristic!.setNotifyValue(true);
       requestCharacteristic.write([1]);
-      _characteristicTimer = Timer(const Duration(seconds: 3), _onCharacteristicTimeout);
+      _characteristicTimer = Timer(const Duration(seconds: 2), _onCharacteristicTimeout);
     });
   }
 
@@ -115,38 +116,6 @@ class _VideoRecorderScreenState extends State<VideoRecorderScreen> {
       });
     }
     _showBluetoothDisconnectedDialog();
-  }
-
-  void _showBluetoothDisconnectedDialog() {
-    // Save session measurements if not already saved
-    if (state.session != null && state.sessionMeasurements != null) {
-      saveToDatabase(state);
-    }
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(2.0),
-          ),
-          title: const Text('Bluetooth Disconnected'),
-          actions: [
-            TextButton(
-              child: const Text('OK'),
-              onPressed: () async {
-                Navigator.of(context).pop();
-                setState(() => _isRecording = false);
-                final route = MaterialPageRoute(
-                  fullscreenDialog: true,
-                  builder: (_) => VideoPage(filePath: _videoFilePath ?? ''),
-                );
-                Navigator.push(context, route);
-              },
-            ),
-          ],
-        );
-      },
-    );
   }
 
   SessionMeasurements createSessionMeasurements(int sessionID) {
@@ -162,9 +131,9 @@ class _VideoRecorderScreenState extends State<VideoRecorderScreen> {
     SessionMeasurements sessionMeasurements = createSessionMeasurements(id);
     //if session measurement is bad and the last one wasn't
     if (sessionMeasurements.value! > state.target!.grip_strength!.toInt() && violating == false) {
-      violating == true;
+      violating = true;
     } else {
-      violating == false;
+      violating = false;
     }
     //add it to a list
     state.setSessionMeasurements(sessionMeasurements);
@@ -199,5 +168,176 @@ class _VideoRecorderScreenState extends State<VideoRecorderScreen> {
         ),
       );
     }
+  }
+
+  void _showBluetoothDisconnectedDialog() {
+    // Save session measurements if not already saved
+    if (state.session != null && state.sessionMeasurements != null) {
+      saveToDatabase(state);
+    }
+
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(2.0),
+          ),
+          title: const Text('Bluetooth Disconnected'),
+          content: const Text('Would you like to retry the connection or end the session?'),
+          actions: [
+            TextButton(
+              child: const Text('Retry Connection'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _retryBluetoothConnection();
+              },
+            ),
+            TextButton(
+              child: const Text('End Session'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                setState(() => _isRecording = false);
+                final route = MaterialPageRoute(
+                  fullscreenDialog: true,
+                  builder: (_) => VideoPage(filePath: _videoFilePath ?? ''),
+                );
+                Navigator.push(context, route);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _retryBluetoothConnection() async {
+    int retryCount = 0;
+    const maxRetries = 3;
+
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return const PopScope(
+          canPop: false,
+          child: AlertDialog(
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                CircularProgressIndicator(),
+                SizedBox(height: 16),
+                Text("Retrying connection..."),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+    while (retryCount < maxRetries) {
+      try {
+        setState(() => isConnecting = true);
+
+        BluetoothDevice? device = state.bluetoothDevice;
+        if (device == null) {
+          throw Exception('Bluetooth device is null');
+        }
+
+        await device.connect(timeout: const Duration(seconds: 30));
+        List<BluetoothService> services = await device.discoverServices(timeout: 30);
+
+        var service = services.where((s) => s.uuid == Guid("19b10000-e8f2-537e-4f6c-d104768a1214")).first;
+        var requestCharacteristic =
+            service.characteristics.where((s) => s.uuid == Guid("19b10001-e8f2-537e-4f6c-d104768a1215")).first;
+        var responseCharacteristic =
+            service.characteristics.where((s) => s.uuid == Guid("19b10001-e8f2-537e-4f6c-d104768a1216")).first;
+        var maxGripStrengthCharacteristic =
+            service.characteristics.where((s) => s.uuid == Guid("19b10001-e8f2-537e-4f6c-d104768a1217")).first;
+        var targetGripPercentageCharacteristic =
+            service.characteristics.where((s) => s.uuid == Guid("19b10001-e8f2-537e-4f6c-d104768a1218")).first;
+        var enableFeedbackCharacteristic =
+            service.characteristics.where((s) => s.uuid == Guid("19b10001-e8f2-537e-4f6c-d104768a1219")).first;
+        var sensorNumberCharacteristic =
+            service.characteristics.where((s) => s.uuid == Guid("19b10001-e8f2-537e-4f6c-d104768a1220")).first;
+        state.targetGripPercentageCharacteristic = targetGripPercentageCharacteristic;
+        state.maxGripStrengthCharacteristic = maxGripStrengthCharacteristic;
+        state.enableFeedbackCharacteristic = enableFeedbackCharacteristic;
+        state.sensorNumberCharacteristic = sensorNumberCharacteristic;
+        if (!completer.isCompleted) {
+          completer.complete();
+        }
+
+        responseCharacteristic.setNotifyValue(true);
+        setState(() => isConnecting = false);
+        await Future.delayed(const Duration(seconds: 5));
+
+        BluetoothConnectionState currentState = await device.connectionState.first;
+        if (currentState == BluetoothConnectionState.connected) {
+          Navigator.of(context).pop();
+          _showReconnectedDialog();
+          return;
+        } else {
+          throw Exception('Failed to establish a stable connection');
+        }
+      } catch (exception) {
+        print('Bluetooth connection attempt failed: $exception');
+        retryCount++;
+
+        if (retryCount >= maxRetries) {
+          Navigator.of(context).pop();
+          _showConnectionFailedDialog();
+          return;
+        }
+        await Future.delayed(const Duration(seconds: 5));
+      }
+    }
+  }
+
+  void _showReconnectedDialog() {
+    if (!mounted) return;
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Bluetooth Reconnected'),
+        content: const Text('The Bluetooth connection has been re-established.'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+              setState(() {
+                _isRecording = false;
+              });
+            },
+            child: const Text('OK'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showConnectionFailedDialog() {
+    if (!mounted) return;
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Bluetooth Connection Failed'),
+        content: const Text('Unable to re-establish the Bluetooth connection after multiple attempts.'),
+        actions: [
+          TextButton(
+            child: const Text('End Session'),
+            onPressed: () {
+              Navigator.of(context).pop();
+              setState(() => _isRecording = false);
+              final route = MaterialPageRoute(
+                fullscreenDialog: true,
+                builder: (_) => VideoPage(filePath: _videoFilePath ?? ''),
+              );
+              Navigator.push(context, route);
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
