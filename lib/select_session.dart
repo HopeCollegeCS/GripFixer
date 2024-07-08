e903c8ae6abb855e8d6d0b78dab5ef3aa8cad6bc
Chris <christopher.laubach@hope.edu>
2024-07-08 08:35:36 -0400

    rerouting

--- lib/select_session.dart
+++ lib/select_session.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:grip_fixer/grip_fixer_drawer.dart';
 import 'package:grip_fixer/grip_target.dart';
-import 'package:grip_fixer/person.dart';
 import 'package:grip_fixer/state.dart';
 import 'package:provider/provider.dart';
 import 'package:grip_fixer/session.dart';
@@ -54,8 +53,7 @@ class _SelectSession extends State<SelectSession> {
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            const Text('Grip Strength Tool',
-                style: TextStyle(color: Color(0xFFFFFFFF))),
+            const Text('Grip Strength Tool', style: TextStyle(color: Color(0xFFFFFFFF))),
             Builder(
               builder: (context) => IconButton(
                 icon: const Icon(Icons.sports_tennis),
@@ -80,8 +78,7 @@ class _SelectSession extends State<SelectSession> {
                     padding: EdgeInsets.symmetric(horizontal: 10),
                     child: Text(
                       'Available Sessions',
-                      style:
-                          TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                      style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
                     ),
                   ),
                   const SizedBox(height: 4),
@@ -95,13 +92,11 @@ class _SelectSession extends State<SelectSession> {
                     rows: sessions?.map((session) {
                           final formattedDate = session.session_date != null
                               ? DateFormat('MMM d').format(
-                                  DateTime.fromMillisecondsSinceEpoch(
-                                      session.session_date),
+                                  DateTime.fromMillisecondsSinceEpoch(session.session_date),
                                 )
                               : 'N/A';
                           return DataRow(
-                            selected:
-                                sessions?.indexOf(session) == selectedValue,
+                            selected: sessions?.indexOf(session) == selectedValue,
                             onSelectChanged: (val) {
                               setState(() {
                                 selectedValue = sessions?.indexOf(session);
@@ -122,16 +117,12 @@ class _SelectSession extends State<SelectSession> {
                     child: ElevatedButton(
                       onPressed: () {
                         context.push("/AnalyzePage");
-                        var state =
-                            Provider.of<AppState>(context, listen: false);
+                        var state = Provider.of<AppState>(context, listen: false);
                         int value = selectedValue!;
                         state.session = sessions?[value];
                         for (int i = 0; i < state.targetMap.length; i++) {
-                          if (state.session?.shot_type ==
-                              state.targetMap.keys.toList()[i]) {
-                            state.target = Target(
-                                grip_strength:
-                                    state.targetMap.values.toList()[i]);
+                          if (state.session?.shot_type == state.targetMap.keys.toList()[i]) {
+                            state.target = Target(grip_strength: state.targetMap.values.toList()[i]);
                           }
                         }
                       },
@@ -143,10 +134,7 @@ class _SelectSession extends State<SelectSession> {
                       ),
                       child: const Text(
                         'Analyze',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                            fontSize: 16),
+                        style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 16),
                       ),
                     ),
                   ),
